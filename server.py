fbe858a85df8b762d1021e73079f3804b60cc25f
R-Mamilla <mamillarakshitha@gmail.com>
2026-02-08 17:20:28 -0500

    Working with postman

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import os
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
@@ -11,18 +11,48 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-with app.app_context():
-  if not os.path.exists("tracker.db"):
-    class Habits(db.Model): 
+class Habits(db.Model): 
       id = db.Column(db.Integer, primary_key=True) 
       name = db.Column(db.String(120), nullable=False)
       created_date = db.Column(db.DateTime, default=db.func.now())
+
+with app.app_context():
+  if not os.path.exists("tracker.db"):
     db.create_all()
     
+
 
 @app.route("/")
 def hello():
   return {"message": "Hello"}
+
+@app.route("/habits", methods = ['POST'])
+def create_item():
+  data = request.get_json()
+
+  if not data:
+    return {"error": "Request body must be JSON"}
+
+  name = data.get("name")
+
+  if not name:
+     return { "error": "name is required"}
+
+  habit = Habits( 
+    name = name
+  )
+
+  db.session.add(habit)
+  db.session.commit()
+  
+  return { "id": habit.id, "name": habit.name, "created_date": habit.created_date }, 201
+
+@app.route("/habits", methods = ['GET'])
+def get_habits():
+  habits = Habits.query.all()
+  return [ { "id": h.id, "name": h.name, "created_date": h.created_date } for h in habits ]
+  
+  
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=50100, debug = True)
