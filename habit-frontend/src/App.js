b3134f2977067f243379550eebe69479ab710dde
R-Mamilla <mamillarakshitha@gmail.com>
2026-02-09 12:56:08 -0500

    Starting with React

---
+++ habit-frontend/src/App.js
@@ -0,0 +1,11 @@
+import AddHabit from "./components/AddHabit";
+import HabitLists from "./components/HabitLists";
+
+function MyApp(){
+  return <div>
+    <AddHabit />
+    <HabitLists />;
+  </div>
+}
+
+export default MyApp
---
+++ habit-frontend/src/components/AddHabit.js
@@ -0,0 +1,6 @@
+export default function AddHabit(){
+  return (<div>
+    <input type="text" />
+    <button>Add a Habit</button>
+  </div>)
+}
